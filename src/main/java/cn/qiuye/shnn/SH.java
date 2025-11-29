639e4caee237dc95c7509c4f598c091c1a2b9cb8
qiuye2024github <qiuye2024github@163.com>
2025-11-29 17:46:05 +0800

    Fixs

--- src/main/java/cn/qiuye/shnn/SH.java
+++ src/main/java/cn/qiuye/shnn/SH.java
@@ -1,22 +1,48 @@
 package cn.qiuye.shnn;
 
 import dev.shadowsoffire.placebo.registry.DeferredHelper;
+
 import net.minecraft.world.level.block.state.BlockBehaviour;
 
 public class SH {
-	private static final DeferredHelper R = DeferredHelper.create(SHNN.MOD_ID);
-	static BlockBehaviour.Properties DEFAULT_STATE = BlockBehaviour.Properties.of()
-			.lightLevel((s) -> 1)
-			.strength(4.0F, 3000.0F).noOcclusion();
 
-	public SH(){}
+    private static final DeferredHelper R = DeferredHelper.create(SHNN.MOD_ID);
+    static BlockBehaviour.Properties DEFAULT_STATE = BlockBehaviour.Properties.of()
+            .lightLevel((s) -> 1)
+            .strength(4.0F, 3000.0F).noOcclusion();
 
-	static void boootstrap() {
-SH.Tabs.bootstrap();
-	}
+    public SH() {}
 
-	public static class Tabs {
-		private static void bootstrap() {
-		}
-	}
+    static void boootstrap() {
+        SH.Blocks.bootstrap();
+        SH.Items.bootstrap();
+        SH.TileEntities.bootstrap();
+        SH.Containers.bootstrap();
+        SH.Tabs.bootstrap();
+    }
+
+    public static class Blocks {
+
+        private static void bootstrap() {}
+    }
+
+    public static class Items {
+
+        private static void bootstrap() {}
+    }
+
+    public static class TileEntities {
+
+        private static void bootstrap() {}
+    }
+
+    public static class Containers {
+
+        private static void bootstrap() {}
+    }
+
+    public static class Tabs {
+
+        private static void bootstrap() {}
+    }
 }
--- src/main/java/cn/qiuye/shnn/SHNN.java
+++ src/main/java/cn/qiuye/shnn/SHNN.java
@@ -1,5 +1,7 @@
 package cn.qiuye.shnn;
 
+import dev.shadowsoffire.placebo.network.MessageHelper;
+
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
@@ -10,37 +12,39 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.network.NetworkRegistry;
 import net.minecraftforge.network.simple.SimpleChannel;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 @Mod(SHNN.MOD_ID)
 public class SHNN {
 
-	public static final String MOD_ID = "shnn";
+    public static final String MOD_ID = "shnn";
     public static final String MOD_NAME = "Super Hosyile Neural Networks";
     public static final Logger LOGGER = LogManager.getLogger(MOD_NAME);
-	public static final SimpleChannel CHANNEL = NetworkRegistry.ChannelBuilder.named(id("shnn"))
-			.clientAcceptedVersions((s) -> true)
-			.serverAcceptedVersions((s) -> true)
-			.networkProtocolVersion(() -> "0.0.1").simpleChannel();
-	public static final ResourceKey<CreativeModeTab> TAB;
+    public static final SimpleChannel CHANNEL = NetworkRegistry.ChannelBuilder.named(id("shnn"))
+            .clientAcceptedVersions((s) -> true)
+            .serverAcceptedVersions((s) -> true)
+            .networkProtocolVersion(() -> "0.0.1").simpleChannel();
+    public static final ResourceKey<CreativeModeTab> TAB;
 
-	public static ResourceLocation id(String id){
-		return ResourceLocation.tryBuild(MOD_ID, id);
-	}
+    public static ResourceLocation id(String id) {
+        return ResourceLocation.tryBuild(MOD_ID, id);
+    }
 
-	public SHNN() {
-		FMLJavaModLoadingContext.get().getModEventBus().register(this);
-		SH.boootstrap();
-	}
+    public SHNN() {
+        FMLJavaModLoadingContext.get().getModEventBus().register(this);
+        SHNNConfig.load();
+        SH.boootstrap();
+        MessageHelper.registerMessage(CHANNEL, 0, new SHNNConfig.ConfigMessage.Provider());
+    }
 
-	@SubscribeEvent
-	public void setup(FMLCommonSetupEvent e) {
-		e.enqueueWork(() -> {
-		});
-	}
+    @SubscribeEvent
+    public void setup(FMLCommonSetupEvent e) {
+        e.enqueueWork(() -> {});
+    }
 
-	static {
-		TAB = ResourceKey.create(Registries.CREATIVE_MODE_TAB, id("tab"));
-	}
+    static {
+        TAB = ResourceKey.create(Registries.CREATIVE_MODE_TAB, id("tab"));
+    }
 }
